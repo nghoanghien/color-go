fa894df55cfb7700ce030587b49823cdd0ee106a
Hien <nguyenhoanghien.2505@gmail.com>
2024-12-13 23:43:26 +0700

    feat: save new user on firestore

--- src/firebase/authenticate.ts
+++ src/firebase/authenticate.ts
@@ -9,18 +9,40 @@ import {
 } from "firebase/auth";
 import app from "./config";
 import { useEffect, useState } from "react";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const provider = new GoogleAuthProvider();
 provider.addScope("https://www.googleapis.com/auth/userinfo.profile");
 
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 export const signIn = () =>
   signInWithPopup(auth, provider)
-    .then((result) => {
+    .then(async (result) => {
       if (!result) return;
 
       const user = result.user;
+
+      const userDoc = {
+        name: user.displayName || "",
+        id: user.uid,
+        favoriteTickets: [],
+        tickets: [],
+        membership: {
+          point: 0,
+          history: []
+        },
+        wallet: {
+          balance: 0,
+          history: []
+        }
+      };
+
+      await setDoc(doc(db, "users", user.uid), userDoc)
+        .catch((error) => {
+          console.error("Error writing document: ", error);
+        });
 
       return user;
     })
@@ -36,6 +58,7 @@ export function useUserInfomation() {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
+        console.log(user);
       }
       setIsLoading(false);
     });
